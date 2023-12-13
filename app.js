0948564aa296eeb394a1923c9ea9d1d4b649d408
Palesa-Tsotetsi <152623456+Palesa-Tsotetsi@users.noreply.github.com>
2023-12-13 17:54:16 +0200

    final working code

--- app.js
+++ app.js
@@ -2,10 +2,45 @@ function updateWeatherValue(response){
     let temperatureElement= document.querySelector("#temperatureValue");
     let temperature = response.data.temperature.current;
     temperatureElement.innerHTML = Math.round(temperature);
+
     let cityName= document.querySelector("#chosenCity");
     cityName.innerHTML= response.data.city;
+
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.condition.description;
+
+    let humidityElement = document.querySelector("#humid");
+    humidityElement.innerHTML= `Humidity: ${response.data.temperature.humidity}%`;
+    
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML= `Wind: ${response.data.wind.speed}km/h`;
+
+    let iconElement = document.querySelector("#icon");
+    iconElement.innerHTML= `<img src="${response.data.condition.icon_url}" class ="icon"/>`;
+
+    let date = new Date(response.data.time * 1000);
+
+    let timeElement = document.querySelector("#time");
+    timeElement.innerHTML= dateFormat(date);
+
+    
+
     console.log(response.data);
 
+}
+function dateFormat(date){
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+
+    let days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day= days[date.getDay()];
+
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+    
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCityData(city){
